aa7f39282235a00ef006262f8638d95a71a71520
Yudai NAKATA <yudai.nkt@gmail.com>
2018-11-19 00:24:06 +0900

    Invoke brew via absolute path
    /usr/local/bin might not be added to PATH at the time of initializing.

--- script/init.sh
+++ script/init.sh
@@ -7,23 +7,23 @@ source ${DOTDIR}/script/lib/util.sh
 # xcode-select --install
 
 # Install Homebrew
-if is_available brew; then
+if is_available /usr/local/bin/brew; then
   echo "Homebrew is already installed."
   exit
 fi
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-if is_available brew; then
-  brew doctor
+if is_available /usr/local/bin/brew; then
+  /usr/local/bin/brew doctor
 else
   echo "Homebrew installation failed."
   exit 1
 fi
 
 # Install Homebrew formulae
-brew bundle --file ${DOTDIR}/script/misc/Brewfile
+/usr/local/bin/brew bundle --file ${DOTDIR}/script/misc/Brewfile
 
 # Change login shell to Homebrewed Zsh
-sudo echo "$(brew --prefix)/bin/zsh" >> /etc/shells
-chsh -s "$(brew --prefix)/bin/zsh"
+sudo echo "$(/usr/local/bin/brew --prefix)/bin/zsh" >> /etc/shells
+chsh -s "$(/usr/local/bin/brew --prefix)/bin/zsh"
