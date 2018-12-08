7ad7ed1edcf2556ed15032f162ab1468fc00edb1
Yudai NAKATA <yudai.nkt@gmail.com>
2018-12-08 23:57:13 +0900

    Install Atom's packages in initialization

--- script/init.sh
+++ script/init.sh
@@ -24,6 +24,18 @@ fi
 # Install Homebrew formulae
 /usr/local/bin/brew bundle --file=${DOTDIR}/script/misc/Brewfile
 
+# Install atom, apm, and Atom packages after Atom.app is successfully installed
+if [[ -d /Applications/Atom.app ]]; then
+  # This is what the windows:install-shell-commands command does in Atom
+  ATOM_RESOURCES_DIR='/Applications/Atom.app/Contents/Resources'
+  ATOM_INSTALL_DIR='/usr/local/bin'
+  ln -sfv ${ATOM_RESOURCES_DIR}/app/atom.sh ${ATOM_INSTALL_DIR}/atom
+  ln -sfv ${ATOM_RESOURCES_DIR}/app/apm/node_modules/.bin/apm ${ATOM_INSTALL_DIR}/apm
+
+  # Install community packages
+  ${ATOM_INSTALL_DIR}/apm install --packages-file ${DOTDIR}/script/misc/atom-packages
+fi
+
 # Change login shell to Homebrewed Zsh
 sudo echo "$(/usr/local/bin/brew --prefix)/bin/zsh" >> /etc/shells
 chsh -s "$(/usr/local/bin/brew --prefix)/bin/zsh"
