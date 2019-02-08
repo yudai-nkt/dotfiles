e8acb426ea075497d4b35fc0f3a819553e1d718d
Yudai NAKATA <yudai.nkt@gmail.com>
2019-02-09 01:30:18 +0900

    Add TeX Live installation procedure

--- script/init.sh
+++ script/init.sh
@@ -36,6 +36,14 @@ if [[ -d /Applications/Atom.app ]]; then
   ${ATOM_INSTALL_DIR}/apm install --packages-file ${DOTDIR}/script/misc/atom-packages
 fi
 
+# Install TeX Live
+curl -L http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz | tar -xz
+cd install-tl-20*
+sudo ./install-tl --profile ${DOTDIR}/script/misc/texlive.profile
+sudo /usr/local/texlive/*/bin/x86_64-darwin/tlmgr path add
+cd ../
+rm -rf install-tl-20*
+
 # Show English directory names in Finder
 for dir in Desktop Documents Downloads Movies Music Pictures Public; do
   rm "${HOME}/${dir}/.localized"
---
+++ script/misc/texlive.profile
@@ -0,0 +1,3 @@
+TEXMFCONFIG ~/.config/texlive/2018
+TEXMFVAR ~/.cache/texlive/2018
+TEXMFHOME ~/.local/share/texlive
