f6bfc163b5a388abb804592bf051c3d8843c57a5
Yudai NAKATA <yudai.nkt@gmail.com>
2024-11-17 03:21:18 +0900

    Add VS Code configuration

--- script/symlink.sh
+++ script/symlink.sh
@@ -13,3 +13,7 @@ for file in ${dotfiles}; do
   fi
   ln -sfnv ${DOTDIR}/${file} ${HOME}/${file}
 done
+
+if is_macos; then
+  ln -sfnv ${DOTDIR}/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+fi
