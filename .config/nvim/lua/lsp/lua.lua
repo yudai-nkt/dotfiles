d39d1a1f830303bc3479a8cea6076ee2faa2ac0b
Yudai NAKATA <yudai.nkt@gmail.com>
2024-11-24 01:10:14 +0900

    Show config file path in notification

--- .config/nvim/lua/lsp/lua.lua
+++ .config/nvim/lua/lsp/lua.lua
@@ -1,11 +1,12 @@
 if vim.fn.exepath("lua-language-server") ~= "" then
   require("lspconfig").lua_ls.setup({})
 else
+  local util = require("util")
   vim.notify(
     "Lua Language Server is not installed",
     vim.log.levels.WARN,
     {
-      title = "dotfiles"
+      title = string.format("Config in %s", util.get_config_path())
     }
   )
 end
---
+++ .config/nvim/lua/util.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+--- This function returns a path of a file where it is *called*.
+--- The returned path is resolved relative to NeoVim config root.
+M.get_config_path = function()
+  local source = debug.getinfo(2, "S").source
+  local config_root_matcher = string.format("%s/(%%w+)", vim.fn.stdpath("config"))
+  return source:sub(1, 1) == "@" and string.gsub(vim.fn.fnamemodify(source:sub(2), ":p"), config_root_matcher, "%1") or nil
+end
+
+return M
