48604c217f542089cecad7e6211fc8fc8db3a509
Yudai NAKATA <yudai.nkt@gmail.com>
2024-11-23 15:09:43 +0900

    Introduce lazy.nvim and catppuccin for NeoVim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  "catppuccin/nvim",
+  tag = "v1.9.0",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    flavour = "frappe",
+    transparent_background = true
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+  end
+}
