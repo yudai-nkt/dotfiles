de4dea05de198bd7e858b00e858ecea1155bfe4a
Yudai NAKATA <yudai.nkt@gmail.com>
2024-11-23 23:46:55 +0900

    Introduce nvim-notify

---
+++ .config/nvim/lua/plugins/notification.lua
@@ -0,0 +1,10 @@
+return {
+  "rcarriga/nvim-notify",
+  tag = "v3.14.0",
+  -- other plugins and my config might send notifications,
+  -- so priority should be higher than the default.
+  priority = 100,
+  config = function()
+    vim.notify = require("notify")
+  end
+}
