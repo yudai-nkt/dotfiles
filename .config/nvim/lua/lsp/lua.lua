75dfec248089e5137641233b13cca0d4f1f15d5b
Yudai NAKATA <yudai.nkt@gmail.com>
2024-11-23 22:05:33 +0900

    Set up minimum LSP configuration

---
+++ .config/nvim/lua/lsp/lua.lua
@@ -0,0 +1,8 @@
+if vim.fn.exepath("lua-language-server") ~= "" then
+  require("lspconfig").lua_ls.setup({})
+else
+  vim.notify(
+    "Lua Language Server is not installed",
+    vim.log.levels.warn
+  )
+end
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,10 @@
+return {
+  "neovim/nvim-lspconfig",
+  tag = "v1.0.0",
+  config = function()
+    local servers = vim.fs.dir(vim.fn.stdpath("config") .. "/lua/lsp/")
+    for name, _ in servers do
+      require(string.format("lsp.%s", string.gsub(name, "(%w+)%.lua", "%1")))
+    end
+  end
+}
