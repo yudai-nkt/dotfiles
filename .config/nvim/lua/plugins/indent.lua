d0c50d8b46d8a8148a424b42f478bd63fd4527a1
Yudai NAKATA <yudai.nkt@gmail.com>
2024-11-25 23:58:28 +0900

    Show indent guide in NeoVim

---
+++ .config/nvim/lua/plugins/indent.lua
@@ -0,0 +1,15 @@
+--- @type LazyPluginSpec | LazyPluginSpec[]
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    tag = "v3.8.5",
+    main = "ibl",
+    --- @module "ibl"
+    --- @type ibl.config
+    opts = {
+      indent = {
+        char = "|"
+      }
+    }
+  }
+}
