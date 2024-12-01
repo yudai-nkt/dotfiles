35032d2f54d708158bdc4e80e4906aeaf5bcd7d3
Yudai NAKATA <yudai.nkt@gmail.com>
2024-12-01 22:57:02 +0900

    Configure clangd for NeoVim

---
+++ .config/nvim/lua/lsp/clangd.lua
@@ -0,0 +1,12 @@
+if vim.fn.exepath("clangd") ~= "" then
+  require("lspconfig").clangd.setup({})
+else
+  local util = require("util")
+  vim.notify(
+    "clangd is not installed.",
+    vim.log.levels.WARN
+    {
+      title = string.format("Config in %s", util.get_config_path())
+    }
+  )
+end
