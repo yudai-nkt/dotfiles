0ccc50edae3d34c88e2d89f7bfa86dd4c9eef64d
Yudai NAKATA <yudai.nkt@gmail.com>
2024-12-05 00:35:29 +0900

    Set up Lua language server with lazydev.nvim

---
+++ .config/nvim/lua/plugins/lazydev.lua
@@ -0,0 +1,16 @@
+--- @type LazyPluginSpec | LazyPluginSpec[]
+return {
+  -- Since vim.api.nvim_get_runtime_file does not look for
+  -- lua plugin sources that are lazy-loaded, this plugin is
+  -- introduced instead of enumerating plugin entries using
+  -- require("lazy").plugins().
+  "folke/lazydev.nvim",
+  opts = {
+    library = {
+      {
+        path = "lazy.nvim",
+        words = { "LazyPluginSpec" },
+      }
+    },
+  },
+}
