dd3559217d509ef6dcb495d1b34790788ca0fa5e
Yudai NAKATA <yudai.nkt@gmail.com>
2024-12-07 11:57:01 +0900

    Add a missing comma in clangd config

--- .config/nvim/lua/lsp/clangd.lua
+++ .config/nvim/lua/lsp/clangd.lua
@@ -2,10 +2,7 @@ if vim.fn.exepath("clangd") ~= "" then
 	require("lspconfig").clangd.setup({})
 else
 	local util = require("util")
-	vim.notify(
-		"clangd is not installed.",
-		vim.log.levels.WARN({
-			title = string.format("Config in %s", util.get_config_path()),
-		})
-	)
+	vim.notify("clangd is not installed.", vim.log.levels.WARN, {
+		title = string.format("Config in %s", util.get_config_path()),
+	})
 end
